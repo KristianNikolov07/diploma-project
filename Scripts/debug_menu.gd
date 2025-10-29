6ee87e490894c653751335fd082166b962462642
KNProNooob <knpronoob@gmail.com>
2025-10-29 14:47:01 +0200

    Added a better debug menu

--- Scripts/debug_menu.gd
+++ Scripts/debug_menu.gd
@@ -1,7 +1,29 @@
-extends Control
+extends CanvasLayer
+
+signal list_inventory_pressed
+signal add_test_item_pressed
+signal remove_test_item_pressed
+signal add_test_consumable_pressed
 
 func _ready() -> void:
+	$Debug.hide()
+
+func _input(event: InputEvent) -> void:
 	if "debug" in OS.get_cmdline_args():
-		show()
-	else:
-		hide()
+		if event.is_action_pressed("Debug"):
+			$Debug.visible = !$Debug.visible
+
+func _on_list_inventory_pressed() -> void:
+	list_inventory_pressed.emit()
+
+
+func _on_add_test_item_pressed() -> void:
+	add_test_item_pressed.emit()
+
+
+func _on_remove_test_item_pressed() -> void:
+	remove_test_item_pressed.emit()
+
+
+func _on_add_test_consumable_pressed() -> void:
+	add_test_consumable_pressed.emit()
