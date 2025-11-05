ec47504899ff0df64ab8c9091b6cb58bf23d1c4d
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-11-05 21:02:56 +0200

    Added trees and tree generation

---
+++ Scripts/world_gen.gd
@@ -0,0 +1,24 @@
+extends Node
+@onready var tilemap : TileMapLayer = get_node("../Tilemap")
+var tree_scene = preload("res://Scenes/Objects/tree.tscn")
+
+
+var grass_tile_atlas_coords = Vector2i(0, 0)
+var sand_tile_atlas_coords = Vector2i(1, 0)
+var water_tile_atlas_coords = Vector2i(2, 0)
+
+@export_range(0, 100, 1) var tree_spawn_change = 10
+
+func _ready() -> void:
+	generate_trees()
+	
+func generate_trees():
+	for x in range(Global.tilemap_size):
+		for y in range(Global.tilemap_size):
+			if tilemap.get_cell_atlas_coords(Vector2i(x, y)) == grass_tile_atlas_coords:
+				var random = randi_range(0, 100)
+				if random <= tree_spawn_change:
+					print("spawn tree")
+					var tree = tree_scene.instantiate()
+					tree.global_position = Global.tilemap_coords_to_global_coords(Vector2(x, y))
+					get_parent().add_child.call_deferred(tree)
