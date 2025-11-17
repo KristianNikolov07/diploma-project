aed745b56c64600e6ef98b5644740edd91ea8f58
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-11-17 21:11:12 +0200

    Changed the ai for the passive entities

---
+++ Scripts/tilemap.gd
@@ -0,0 +1,17 @@
+extends TileMapLayer
+var add_navigation_to_tiles : Array[Vector2i]
+var remove_navigation_from_tiles : Array[Vector2i]
+
+func _use_tile_data_runtime_update(coords: Vector2i):
+	if coords in remove_navigation_from_tiles or coords in add_navigation_to_tiles:
+		return true
+	return false
+
+func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData):
+	print(coords)
+	if coords in remove_navigation_from_tiles:
+		tile_data.set_navigation_polygon(0, null)
+		remove_navigation_from_tiles.erase(coords)
+	elif coords in add_navigation_to_tiles:
+		tile_data.set_navigation_polygon(0, NavigationPolygon.new())
+		add_navigation_to_tiles.erase(coords)
