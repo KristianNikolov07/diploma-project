f08ae0f5a4ed81624d47defc5fd60cfc8c1071e8
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-11-17 21:17:13 +0200

    Changed the ai for the enemies

--- Scripts/Classes/Entities/Enemy.gd
+++ Scripts/Classes/Entities/Enemy.gd
@@ -4,23 +4,12 @@ extends Entity
 @export var dmg : int
 
 func _ready():
-	$NavigationAgent2D.path_desired_distance = 4.0
-	$NavigationAgent2D.target_desired_distance = 4.0
 	if $ContactDamage != null:
 		$ContactDamage.body_entered.connect(_contact_damage_on_body_entered)
-	
 
-func retarget():
-	$NavigationAgent2D.target_position = get_tree().get_first_node_in_group("Player").global_position
+func _physics_process(_delta):
 
-func _physics_process(delta):
-	retarget()
-	if $NavigationAgent2D.is_navigation_finished():
-		return
-	var current_agent_position: Vector2 = global_position
-	var next_path_position: Vector2 = $NavigationAgent2D.get_next_path_position()
-
-	velocity = current_agent_position.direction_to(next_path_position) * speed
+	velocity = global_position.direction_to(get_tree().get_first_node_in_group("Player").global_position) * speed
 	move_and_slide()
 
 func _contact_damage_on_body_entered(body):
