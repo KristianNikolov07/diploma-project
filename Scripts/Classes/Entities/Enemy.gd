1c760217e72f9fca87b55e42d58e6896781449e1
KristianNikolov07 <kris.nikolov07@gmail.com>
2025-12-05 18:12:41 +0200

    Made enemies go idle when a set amount of distance from the player

--- Scripts/Classes/Entities/Enemy.gd
+++ Scripts/Classes/Entities/Enemy.gd
@@ -2,6 +2,7 @@ class_name Enemy
 extends Entity
 
 @export var dmg : int
+@export var activate_distance : float
 
 func _ready() -> void:
 	if $ContactDamage != null:
@@ -9,8 +10,9 @@ func _ready() -> void:
 
 
 func _physics_process(_delta) -> void:
-	velocity = global_position.direction_to(get_tree().get_first_node_in_group("Player").global_position) * speed
-	move_and_slide()
+	if global_position.distance_to(get_tree().get_first_node_in_group("Player").global_position) < activate_distance:
+		velocity = global_position.direction_to(get_tree().get_first_node_in_group("Player").global_position) * speed
+		move_and_slide()
 
 
 func _contact_damage_on_body_entered(body) -> void:
