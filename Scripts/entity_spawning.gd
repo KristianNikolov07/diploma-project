64910aa0a85a259ad1493e7e91493708ba12a6c8
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-12-16 17:18:57 +0200

    Entities no longer spawn inside of collisions

--- Scripts/entity_spawning.gd
+++ Scripts/entity_spawning.gd
@@ -6,8 +6,21 @@ extends Node
 @export var max_distance_from_player : float
 @export var spawn_amount = 5
 
+var spawn_check_scene = preload("res://Scenes/Systems/spawn_check.tscn")
+
 @onready var day_night_cycle: DirectionalLight2D = $"../DayNightCycle"
 @onready var player: Player = %Player
+
+func check_position(pos : Vector2) -> bool:
+	var spawn_check : Area2D = spawn_check_scene.instantiate()
+	spawn_check.global_position = pos
+	get_tree().current_scene.add_child(spawn_check)
+	if spawn_check.get_overlapping_bodies().is_empty():
+		spawn_check.queue_free()
+		return true
+	else:
+		spawn_check.queue_free()
+		return false
 
 
 func _on_spawn_attempt_timeout() -> void:
@@ -18,7 +31,7 @@ func _on_spawn_attempt_timeout() -> void:
 		entity_scene = passiveEntityes.pick_random()
 	
 	for i in range(spawn_amount):
-		var entity = entity_scene.instantiate()
+		# Choose Position
 		var distance : Vector2
 		distance.x = randf_range(min_distance_from_player, max_distance_from_player)
 		distance.y = randf_range(min_distance_from_player, max_distance_from_player)
@@ -26,6 +39,8 @@ func _on_spawn_attempt_timeout() -> void:
 			distance.x = -distance.x
 		if randi_range(0, 1) == 0:
 			distance.y = -distance.y
-		entity.global_position = player.global_position + distance
-		
-		get_parent().add_child(entity)
+		var position : Vector2 = player.global_position + distance
+		if check_position(position):
+			var entity = entity_scene.instantiate()
+			entity.global_position = position
+			get_parent().add_child(entity)
