b7cab3ca369ca3c25e97af2c763f29cc571684da
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-12-20 11:40:20 +0200

    Added water animation

--- Scripts/tilemap.gd
+++ Scripts/tilemap.gd
@@ -1,28 +1,28 @@
 extends TileMapLayer
 
-var grass_tile_atlas_coords = [Vector2i(1, 1)]
-var sand_tile_atlas_coords = [Vector2i(4, 1), Vector2i(1, 4)]
-var water_tile_atlas_coords = [Vector2i(4, 4)]
 
 func is_water_tile(pos : Vector2i) -> bool:
-	if get_cell_atlas_coords(pos) in water_tile_atlas_coords:
-		return true
-	else:
-		return false
+	var data = get_cell_tile_data(pos)
+	if data:
+		if data.get_custom_data("is_water"):
+			return true
+	return false
 
 
 func is_sand_tile(pos : Vector2i) -> bool:
-	if get_cell_atlas_coords(pos) in sand_tile_atlas_coords:
-		return true
-	else:
-		return false
+	var data = get_cell_tile_data(pos)
+	if data:
+		if data.get_custom_data("is_sand"):
+			return true
+	return false
 
 
 func is_grass_tile(pos : Vector2i) -> bool:
-	if get_cell_atlas_coords(pos) in grass_tile_atlas_coords:
-		return true
-	else:
-		return false
+	var data = get_cell_tile_data(pos)
+	if data:
+		if data.get_custom_data("is_grass"):
+			return true
+	return false
 
 
 func interact(player : Player):
