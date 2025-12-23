6b57302e73251f678639f5c5367bff6d0f46b858
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-12-23 17:29:51 +0200

    The settings now save properly

--- Scripts/Menu/settings_menu.gd
+++ Scripts/Menu/settings_menu.gd
@@ -2,8 +2,21 @@ extends Control
 
 signal closed
 
+const SETTINGS_FILE_PATH = "user://settings.ini"
+
+var config = ConfigFile.new()
+
 func _ready() -> void:
 	hide()
+	
+	# Get Settings from file
+	config.load(SETTINGS_FILE_PATH)
+	var lang = config.get_value("Settings", "Language", 0)
+	var volume = config.get_value("Settings", "Volume", 100)
+	set_language(lang)
+	$Settings/Language/Language.selected = lang
+	set_volume(volume)
+	$Settings/Volume/HSlider.value = volume
 
 
 func _on_back_pressed() -> void:
@@ -12,14 +25,28 @@ func _on_back_pressed() -> void:
 
 
 func _on_language_selected(index: int) -> void:
-	if index == 0:
+	config.load(SETTINGS_FILE_PATH)
+	config.set_value("Settings", "Language", index)
+	config.save(SETTINGS_FILE_PATH)
+	set_language(index)
+
+
+func set_language(lang : int):
+	if lang == 0:
 		pass
-	elif index == 1:
+	elif lang == 1:
 		pass
 
 
-func _on_volume_changed() -> void:
-	AudioServer.set_bus_volume_linear(0, $Settings/Volume/HSlider.value)
+func _on_volume_changed(value : float) -> void:
+	config.load(SETTINGS_FILE_PATH)
+	config.set_value("Settings", "Volume", value)
+	config.save(SETTINGS_FILE_PATH)
+	set_volume(value)
+
+
+func set_volume(volume : float):
+	AudioServer.set_bus_volume_linear(0, volume)
 
 
 func _on_mods_pressed() -> void:
