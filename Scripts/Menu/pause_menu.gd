dc0c85dfc1e1757473bc1fc0eb6e9b740ce6ff4a
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-12-23 14:05:16 +0200

    Added a pause menu

---
+++ Scripts/Menu/pause_menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var player : Player = get_node("../../")
+
+func _ready() -> void:
+	hide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("esc"):
+		if visible:
+			_on_resume_pressed()
+		elif player.can_move:
+			get_tree().paused = true
+			show()
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	hide()
+	$Buttons.show()
+	$SettingsMenu.hide()
+
+
+func _on_settings_pressed() -> void:
+	$Buttons.hide()
+	$SettingsMenu.show()
+
+
+func _on_save_and_quit_pressed() -> void:
+	SaveProgress.save()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
+
+
+func _on_settings_menu_closed() -> void:
+	$Buttons.show()
