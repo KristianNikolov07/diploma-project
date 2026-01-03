6dc0eacbd57d7fb9e2288097d283a155f823c7be
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2026-01-03 23:47:50 +0200

    Cleanup

--- Scripts/Menu/main_menu.gd
+++ Scripts/Menu/main_menu.gd
@@ -2,7 +2,8 @@ extends Control
 
 func _ready() -> void:
 	SaveProgress.save_name = "" # So it doesn't try to save the game while in the main menu
-
+	
+	$Version.text = ProjectSettings.get_setting("application/config/version")
 
 func _on_play_pressed() -> void:
 	$MainButtons.hide()
