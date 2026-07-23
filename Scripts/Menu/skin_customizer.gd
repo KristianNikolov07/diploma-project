6853b7cdecf6528db442147d3a8df6ac87047278
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2026-07-23 20:14:32 +0300

    Changed the color picker sliders

--- Scripts/Menu/skin_customizer.gd
+++ Scripts/Menu/skin_customizer.gd
@@ -15,10 +15,13 @@ func open() -> void:
 
 
 func set_to_default_colors() -> void:
-	%HairSliders.set_color(%PlayerSprite.DEFAULT_HAIR_COLOR)
-	%SkinSliders.set_color(%PlayerSprite.DEFAULT_SKIN_COLOR)
-	%ShirtSliders.set_color(%PlayerSprite.DEFAULT_SHIRT_COLOR)
-
+	%HairColor.color = %PlayerSprite.DEFAULT_HAIR_COLOR
+	%PlayerSprite.set_hair_color(%PlayerSprite.DEFAULT_HAIR_COLOR)
+	%SkinColor.color = %PlayerSprite.DEFAULT_SKIN_COLOR
+	%PlayerSprite.set_skin_color(%PlayerSprite.DEFAULT_SKIN_COLOR)
+	%ShirtColor.color = %PlayerSprite.DEFAULT_SHIRT_COLOR
+	%PlayerSprite.set_shirt_color(%PlayerSprite.DEFAULT_SHIRT_COLOR)
+	set_unsaved_changes(true)
 
 func load_from_file() -> void:
 	var config = ConfigFile.new()
@@ -28,9 +31,12 @@ func load_from_file() -> void:
 	var hair = config.get_value("colors", "hair_color", %PlayerSprite.DEFAULT_HAIR_COLOR)
 	var skin = config.get_value("colors", "skin_color", %PlayerSprite.DEFAULT_SKIN_COLOR)
 	var shirt = config.get_value("colors", "shirt_color", %PlayerSprite.DEFAULT_SHIRT_COLOR)
-	%HairSliders.set_color(hair)
-	%SkinSliders.set_color(skin)
-	%ShirtSliders.set_color(shirt)
+	%HairColor.color = hair
+	%PlayerSprite.set_hair_color(hair)
+	%SkinColor.color = skin
+	%PlayerSprite.set_skin_color(skin)
+	%ShirtColor.color = shirt
+	%PlayerSprite.set_shirt_color(shirt)
 	set_unsaved_changes(false)
 
 
@@ -42,9 +48,9 @@ func set_unsaved_changes(_unsaved_changes : bool) -> void:
 func _on_save_pressed() -> void:
 	var config = ConfigFile.new()
 	config.load(Global.SKIN_CUSTOMIZATIONS_FILE_PATH)
-	config.set_value("colors", "hair_color", %HairSliders.get_color())
-	config.set_value("colors", "skin_color", %SkinSliders.get_color())
-	config.set_value("colors", "shirt_color", %ShirtSliders.get_color())
+	config.set_value("colors", "hair_color", %HairColor.color)
+	config.set_value("colors", "skin_color", %SkinColor.color)
+	config.set_value("colors", "shirt_color", %ShirtColor.color)
 	config.save(Global.SKIN_CUSTOMIZATIONS_FILE_PATH)
 	set_unsaved_changes(false)
 
@@ -58,13 +64,16 @@ func _on_close_pressed() -> void:
 	close.emit()
 
 
-func _on_hair_sliders_color_changed() -> void:
+func _on_hair_color_color_changed(color: Color) -> void:
+	%PlayerSprite.set_hair_color(color)
 	set_unsaved_changes(true)
 
 
-func _on_skin_sliders_color_changed() -> void:
+func _on_skin_color_color_changed(color: Color) -> void:
+	%PlayerSprite.set_skin_color(color)
 	set_unsaved_changes(true)
 
 
-func _on_shirt_sliders_color_changed() -> void:
+func _on_shirt_color_color_changed(color: Color) -> void:
+	%PlayerSprite.set_shirt_color(color)
 	set_unsaved_changes(true)
